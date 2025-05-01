4dfd57689cfb1fd662abedea2eee2d8c24e690e0
VedantMaheshwari28 <vedantmaheshwari2006@gmail.com>
2025-05-02 00:26:13 +0530

    Delete Eagle.java

--- Eagle.java
+++
@@ -1,6 +0,0 @@
-public class Eagle extends Bird implements Interfaces.Message{
-	public void sendMessage(String message) {
-		System.out.println("Eagle Flew away with message tied on his foot contaning "+ message);
-	}
-	
-}
